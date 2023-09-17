27c90f859bb3e6be2cdb9c11f922810e705db8f6
CristinaM92 <139230557+CristinaM92@users.noreply.github.com>
2023-09-17 16:11:33 +0000

    added output

--- src/input.py
+++ src/input.py
@@ -1,10 +1,15 @@
-from extract import get_input, remove_duplicates
+from extract import *
 
 def main():
     filename = 'results.csv'
     input = get_input(filename)
     input_deduped = remove_duplicates(input) 
-    print(input_deduped)
+    removed_blanks = remove_empty_lines(input_deduped)
+    capitalised_names = capitalize_names(removed_blanks)
+    valid_ans_3 = validate_answer_3(capitalised_names)
+    output_to_file("clean_results.csv",valid_ans_3)
+
+    
 
 if  __name__ == '__main__':
     main()
